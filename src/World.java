17344bc169441d13dae091c74e8c4a3e8ea642eb
Zach Roberts <zach.ker.roberts@gmail.com>
2016-08-29 19:45:59 -0400

    all fixed

--- src/World.java
+++ src/World.java
@@ -9,40 +9,44 @@ public class World {
 	public static double games;
 
 	public static void main(String[] args) {
-		for(int i = 0; i < 1000; i++){
-		World world = new World();
-		Player player1 = new Player();
-		Dealer dealer = new Dealer();
-		dealer.dealCards();
-		world.hitOrStay(dealer);
-		world.dealerHit(dealer);
-		world.determineWinners(dealer, player1);
+		for (int i = 0; i < 2000; i++) {
+			World world = new World();
+			Player player1 = new Player();
+			Dealer dealer = new Dealer();
+			dealer.dealCards();
+			world.hitOrStay(dealer, player1);
+			world.dealerHit(dealer);
+			world.determineWinners(dealer, player1);
 		}
-		 System.out.println("ties percantage: " + (World.ties/World.games) * 100);
-		 System.out.println("wins percentage: " + (World.wins/World.games) * 100);
-		 System.out.println("losses percentage: " + (World.losses/World.games) * 100);
-		 System.out.println("errors " + World.errors);
-		 System.out.println("games " + World.games);
+		System.out.println("ties percantage: " + (World.ties / World.games) * 100);
+		System.out.println("wins percentage: " + (World.wins / World.games) * 100);
+		System.out.println("losses percentage: " + (World.losses / World.games) * 100);
+		System.out.println("errors " + World.errors);
+		System.out.println("games " + World.games);
 
 	}
 
-	public void hitOrStay(Dealer dealer) {
-		for (Player player : Player.players) {
+	public void hitOrStay(Dealer dealer, Player player) {
+
+		do {
 			player.calcTotal();
 			if (player.calcTotal() < 11) {
+				
 				player.hit(dealer);
 			} else if ((player.calcTotal() > 10 && player.calcTotal() < 17)
-					&& (dealer.firstCard.getValue() < 7 && dealer.firstCard.getValue() > 1)) {
+					&& (dealer.firstCard.getValue() < 12 && dealer.firstCard.getValue() > 6)) {
 				player.hit(dealer);
-			} else {
+			} else if ((player.calcTotal() > 10 && player.calcTotal() < 17) 
+					&& (dealer.firstCard.value < 7 && dealer.firstCard.value > 1)){
+				break;
+			} else{
 				break;
 			}
-
-		}
+		}while(player.calcTotal() < 17);
 	}
 
 	public void dealerHit(Dealer dealer) {
-//		System.out.println(dealer.calcTotal());
+		// System.out.println(dealer.calcTotal());
 		if (dealer.calcTotal() < 17) {
 			do {
 				dealer.hit();
@@ -51,24 +55,24 @@ public class World {
 	}
 
 	public void determineWinners(Dealer dealer, Player player) {
-//		for (Player player : Player.players) {
-			String outcome = player.determineWin(dealer);
-			switch (outcome) {
-			case "tie":
-				World.ties++;
-				break;
-			case "error":
-				World.errors++;
-				break;
-			case "true":
-				World.wins++;
-				break;
-			case "false":
-				World.losses++;
-				break;
-			}
+		// for (Player player : Player.players) {
+		String outcome = player.determineWin(dealer);
+		switch (outcome) {
+		case "tie":
+			World.ties++;
+			break;
+		case "error":
+			World.errors++;
+			break;
+		case "true":
+			World.wins++;
+			break;
+		case "false":
+			World.losses++;
+			break;
+		}
 
-//		}
+		// }
 		World.games++;
 
 	}
